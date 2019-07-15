24079d449e50616e112fdf310e57ac2b53d31dfc
Ryan Silva <RyandotSilva@gmail.com>
2019-07-15 16:06:53 -0400

    Add logic to display logs

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -2,12 +2,12 @@ import React, { useState, useEffect } from "react";
 
 const Logs = () => {
 	const [logs, setLogs] = useState([]);
-  const [loading, setLoading] = useState(false);
-  
-  useEffect(() => {
-    getLogs();
-    // eslint-disable-next-line
-  }, [])
+	const [loading, setLoading] = useState(false);
+
+	useEffect(() => {
+		getLogs();
+		// eslint-disable-next-line
+	}, []);
 
 	const getLogs = async () => {
 		setLoading(true);
@@ -23,7 +23,17 @@ const Logs = () => {
 	}
 
 	return (
-
-  );
+		<ul className="collection-with-header">
+			<li className="collection-header">
+				<h4 className="center">System Logs</h4>
+			</li>
+			{!loading && logs.length === 0 ? (
+				<p className="center">No logs to show...</p>
+			) : (
+				logs.map(log => <li>{log.message}</li>)
+			)}
+		</ul>
+	);
+};
 
 export default Logs;
