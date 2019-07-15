e90beae5b69400af0d99431f0958072e4bf84b02
Ryan Silva <RyandotSilva@gmail.com>
2019-07-15 16:02:49 -0400

    Add getLogs function

--- src/components/logs/Logs.js
+++ src/components/logs/Logs.js
@@ -4,6 +4,15 @@ const Logs = () => {
 	const [logs, setLogs] = useState([]);
 	const [loading, setLoading] = useState(false);
 
+	const getLogs = async () => {
+		setLoading(true);
+		const res = await fetch("/logs");
+		const data = await res.json();
+
+		setLogs(data);
+		setLoading(false);
+	};
+
 	return <div />;
 };
 
