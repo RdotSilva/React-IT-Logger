7f900c08f7780f243f76a59a3d22e20b981f4fc8
Ryan Silva <RyandotSilva@gmail.com>
2019-07-15 15:58:33 -0400

    Change class to classNames to work with React

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -2,18 +2,20 @@ import React from "react";
 
 const SearchBar = () => {
 	return (
-    <nav style={{marginBottom: '30px'}} className="blue">
-    <div class="nav-wrapper">
-      <form>
-        <div class="input-field">
-          <input id="search" type="search" required />
-          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-          <i class="material-icons">close</i>
-        </div>
-      </form>
-    </div>
-  </nav>
-
-  )
+		<nav style={{ marginBottom: "30px" }} className="blue">
+			<div className="nav-wrapper">
+				<form>
+					<div className="input-field">
+						<input id="search" type="search" required />
+						<label className="label-icon" htmlFor="search">
+							<i className="material-icons">search</i>
+						</label>
+						<i className="material-icons">close</i>
+					</div>
+				</form>
+			</div>
+		</nav>
+	);
+};
 
 export default SearchBar;
