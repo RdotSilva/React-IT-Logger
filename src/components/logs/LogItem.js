e26d96bf1007b75dc32f084c646401d39a39cb14
Ryan Silva <RyandotSilva@gmail.com>
2019-07-15 16:22:27 -0400

    Update LogItem to show date using moment

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -18,6 +18,7 @@ const LogItem = ({ log }) => {
 				<span className="grey-text">
 					<span className="black-text">ID # {log.id}</span> last updated by{" "}
 					<span className="black-text">{log.tech}</span> on{" "}
+					<Moment format="MMMM Do YYYY, h:mm:ss a">{log.date}</Moment>
 				</span>
 			</div>
 		</li>
