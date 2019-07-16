76c758cb686e56549e8500d713cba8f6a90e4b13
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 18:40:45 -0400

    Add logic to map and display techs

--- src/components/techs/TechListModal.js
+++ src/components/techs/TechListModal.js
@@ -4,12 +4,12 @@ const TechListModal = () => {
 	const [techs, setTechs] = useState([]);
 	const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
+	useEffect(() => {
 		getTechs();
 		// eslint-disable-next-line
 	}, []);
 
-  const getTechs = async () => {
+	const getTechs = async () => {
 		setLoading(true);
 		const res = await fetch("/techs");
 		const data = await res.json();
@@ -18,14 +18,23 @@ const TechListModal = () => {
 		setLoading(false);
 	};
 
-	
-  if (loading) {
+	if (loading) {
 		return <h4>Loading...</h4>;
 	}
-	
+
 	return (
-		
+		<div id="tech-list-modal" className="modal">
+			<div className="modal-content">
+				<h4>Technician List</h4>
+				<ul className="collection">
+					{!loading &&
+						techs.map(tech => (
+							<li className="collection-item">{tech.firstName}</li>
+						))}
+				</ul>
+			</div>
+		</div>
 	);
 };
 
-export default Logs;
+export default TechListModal;
