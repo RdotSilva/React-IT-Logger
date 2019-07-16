ba51b5ed42dfb0cdadea4e6a632dac9b9128ea24
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 18:37:19 -0400

    Set up TechListModal state

---
+++ src/components/techs/TechListModal.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+
+const TechListModal = () => {
+	const [techs, setTechs] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+
+	return (
+		
+	);
+};
+
+export default Logs;
