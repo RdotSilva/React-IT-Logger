df63028ec7276af36b10f89040c1f7ec8a5751b2
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 18:19:15 -0400

    Add onSubmit function

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -4,6 +4,21 @@ import M from "materialize-css/dist/js/materialize.min.js";
 const AddTechModal = () => {
 	const [firstName, setFirstName] = useState("");
 	const [lastName, setLastName] = useState("");
+
+	const onSubmit = () => {
+		if (firstName === "" || lastName === "") {
+			M.toast({
+				html: "Please enter a the first and last name"
+			});
+		} else {
+			console.log(firstName, lastName);
+			setFirstName("");
+			setLastName("");
+		}
+	};
+
+	return (
+  )
 };
 
 const modalStyle = {
