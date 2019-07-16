5f2c390de79efa88668c00a890348f6152712c3f
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 18:17:56 -0400

    Set up state in AddTechModal component

---
+++ src/components/techs/AddTechModal.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+
+const AddTechModal = () => {
+	const [firstName, setFirstName] = useState("");
+	const [lastName, setLastName] = useState("");
+};
+
+const modalStyle = {
+	width: "75%",
+	height: "75%"
+};
+
+export default AddTechModal;
