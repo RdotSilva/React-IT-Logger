46dc0d28e711e602bbc79c6ece38e5100637f756
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 18:22:22 -0400

    Add first name input field

--- src/components/techs/AddTechModal.js
+++ src/components/techs/AddTechModal.js
@@ -20,54 +20,21 @@ const AddTechModal = () => {
 	return (
 		<div id="add-tech-modal" className="modal">
 			<div className="modal-content">
-				<h4>Enter System Log</h4>
+				<h4>New Technician</h4>
 				<div className="row">
 					<div className="input-field">
 						<input
 							type="text"
-							name="message"
-							value={message}
-							onChange={e => setMessage(e.target.value)}
+							name="firstName"
+							value={firstName}
+							onChange={e => setFirstName(e.target.value)}
 						/>
-						<label htmlFor="message" className="active">
-							Log Message
+						<label htmlFor="firstName" className="active">
+							First Name
 						</label>
 					</div>
 				</div>
-				<div className="row">
-					<div className="input-field">
-						<select
-							name="tech"
-							value={tech}
-							className="browser-default"
-							onChange={e => setTech(e.target.value)}
-						>
-							<option value="" disabled>
-								Select Technician
-							</option>
-							<option value="John Doe">John Doe</option>
-							<option value="Sam Smith">Sam Smith</option>
-							<option value="Sara Wilson">Sara Wilson</option>
-						</select>
-					</div>
-				</div>
-				<div className="row">
-					<div className="input-field">
-						<p>
-							<label>
-								<input
-									type="checkbox"
-									className="filled-in"
-									checked={attention}
-									value={attention}
-									onChange={e => setAttention(!attention)}
-								/>
-								<span>Needs Attention</span>
-							</label>
-						</p>
-					</div>
-				</div>
-			</div>
+				
 			<div className="modal-footer">
 				<a
 					href="#!"
