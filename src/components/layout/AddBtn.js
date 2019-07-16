e69cfbe33356386241486b932298ef5e85308ca7
Ryan Silva <RyandotSilva@gmail.com>
2019-07-16 17:38:03 -0400

    Add tech modal

--- src/components/layout/AddBtn.js
+++ src/components/layout/AddBtn.js
@@ -7,7 +7,7 @@ const AddBtn = () => {
 				href="#add-log-modal"
 				className="btn-floating btn-large blue darken-2 modal-trigger"
 			>
-				<i className="large material-icons">Add</i>
+				<i className="large material-icons">add</i>
 			</a>
 			<ul>
 				<li>
@@ -15,7 +15,12 @@ const AddBtn = () => {
 						href="#tech-list-modal"
 						className="btn-floating green modal-trigger"
 					>
-						<i className="material-icons">Person</i>
+						<i className="material-icons">person</i>
+					</a>
+				</li>
+				<li>
+					<a href="#tech-modal" className="btn-floating red modal-trigger">
+						<i className="material-icons">person_add</i>
 					</a>
 				</li>
 			</ul>
