c91edd9256043a9a24f1e00ddcbab1fe0d8c5131
Ryan Silva <RyandotSilva@gmail.com>
2019-07-18 09:33:02 -0400

    Update getLogs action to dispatch

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,7 +1,19 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 // Fetch logs from server
-export const getLogs = () => {};
+export const getLogs = () => {
+	return async dispatch => {
+		setLoading();
+
+		const res = await fetch("/logs");
+		const data = await res.json();
+
+		dispatch({
+			type: GET_LOGS,
+			payload: data
+		});
+	};
+};
 
 // Set loading to true
 export const setLoading = () => {
