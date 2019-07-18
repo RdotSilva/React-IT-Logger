8b2db35325ab7636100a63a2536fc802982a9683
Ryan Silva <RyandotSilva@gmail.com>
2019-07-18 11:18:26 -0400

    Update search input field

--- src/components/layout/SearchBar.js
+++ src/components/layout/SearchBar.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { searchLogs } from "../../actions/logActions";
 
 const SearchBar = ({ searchLogs }) => {
+	const text = useRef("");
+
 	return (
 		<nav style={{ marginBottom: "30px" }} className="blue">
 			<div className="nav-wrapper">
 				<form>
 					<div className="input-field">
-						<input id="search" type="search" required />
+						<input
+							id="search"
+							type="search"
+							placeholder="Search Logs.."
+							ref={text}
+							onChange={onChange}
+						/>
 						<label className="label-icon" htmlFor="search">
 							<i className="material-icons">search</i>
 						</label>
