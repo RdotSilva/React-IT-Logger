9359eb6c6b8bd5414382148ebdfee36db1fc5651
Ryan Silva <RyandotSilva@gmail.com>
2019-07-18 11:00:30 -0400

    Add update log action

--- src/reducers/logReducer.js
+++ src/reducers/logReducer.js
@@ -36,6 +36,13 @@ export default (state = initialState, action) => {
 				logs: state.logs.filter(log => log.id !== action.payload),
 				loading: false
 			};
+		case UPDATE_LOG:
+			return {
+				...state,
+				logs: state.logs.map(log =>
+					log.id === action.payload.id ? action.payload : log
+				)
+			};
 		case SET_CURRENT:
 			return {
 				...state,
