4a51cb947d64576290fdbadb01aadc50cf559810
Ryan Silva <RyandotSilva@gmail.com>
2019-07-18 09:35:14 -0400

    Refactor getLogs to use try catch

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,8 +1,23 @@
 import { GET_LOGS, SET_LOADING, LOGS_ERROR } from "./types";
 
 // Fetch logs from server
-export const getLogs = () => {
-	return async dispatch => {
+// export const getLogs = () => {
+// 	return async dispatch => {
+// 		setLoading();
+
+// 		const res = await fetch("/logs");
+// 		const data = await res.json();
+
+// 		dispatch({
+// 			type: GET_LOGS,
+// 			payload: data
+// 		});
+// 	};
+// };
+
+// Fetch logs from server
+export const getLogs = () => async dispatch => {
+	try {
 		setLoading();
 
 		const res = await fetch("/logs");
@@ -12,7 +27,12 @@ export const getLogs = () => {
 			type: GET_LOGS,
 			payload: data
 		});
-	};
+	} catch (err) {
+		dispatch({
+			type: LOGS_ERROR,
+			payload: err.response.data
+		});
+	}
 };
 
 // Set loading to true
